1b944f63f3121651cc8c82172f3ba4631ae34849
G-boy75 <glul71597@gmail.com>
2022-04-07 13:15:23 -0400

    Component State

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import './App.css';
 // I want you to make this class called App
 // and I want you to extend the fuctionality of this  { Component } class that react has given us
 class App extends Component {
+  // In class base component the way to access or generate the state is through constructor method
+  constructor() {
+    super();
+    // instantiate the state object
+    this.state = {
+      name: 'Felix Andrew'
+    }
+  }
   // it also creates this render() method for class base components
   render() {
     // What we are passing in render() method is this return statement
@@ -15,16 +23,11 @@ class App extends Component {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {/* referencing our state and referencing the key to get the name value */}
+        {/* now this paragraph tag is no longer static it is now dynamic  */}
+          Hello {this.state.name}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>Change Name</button>
       </header>
     </div>
   );
