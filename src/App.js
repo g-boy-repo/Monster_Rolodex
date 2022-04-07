381527d4d6d9229679c244a07ac3a0fc33912888
G-boy75 <glul71597@gmail.com>
2022-04-06 22:52:34 -0400

    Component State

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+// By importing this it allows us to use class Component built in functionalities
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+// I want you to make this class called App
+// and I want you to extend the fuctionality of this  { Component } class that react has given us
+class App extends Component {
+  // it also creates this render() method for class base components
+  render() {
+    // What we are passing in render() method is this return statement
+    return (
+    // Inside here are JSX that tells the browser how to render the page
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +28,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
