dddc8943f2200c09d479d299c39417349c179f73
G-boy75 <glul71597@gmail.com>
2022-05-11 16:53:54 -0400

    Adding CardList Component

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class CardList extends Component {
+    render() {
+        return (
+        <div>Hello I'm the CardList Components</div>
+        );
+    }
+}
+
+
+export default CardList;
